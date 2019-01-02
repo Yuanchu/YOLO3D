68885b095595961c3e6eaf7cbfde94b21dc7b4ca
Yuanchu <yuanchu.dang@gmail.com>
2019-01-02 04:23:46 +0000

    refactor generate_train_txt

--- generate_train_txt.py
+++ generate_train_txt.py
@@ -3,18 +3,33 @@
 """
 
 
-from os import listdir
-from os.path import isfile, join
+import argparse
+import os
 
-# Grab all files and determine training size
-path = '/home/yd2466/Complex-YOLO/data/training/calib/'
-files = [f for f in listdir(path) if isfile(join(path, f))]
-train_ratio = 0.85
-train_size = min(6000, int(len(files) * train_ratio))
+if __name__ == "__main__":
+    
+    # Train ratio and max size from cmd
+    parser = argparse.ArgumentParser(prog = 'Generate training idx.')
+    parser.add_argument('train_ratio', type = float, help = 'Training ratio.')
+    parser.add_argument('max_size', type = int, help = 'Maximum number of training images.')
+    args = parser.parse_args()
+    train_ratio = args.train_ratio
+    max_size = args.max_size
 
-# Write to train.txt 
-file = open('/home/yd2466/Complex-YOLO/data/training/train.txt', 'w') 
-for i in range(train_size):
-	file.write(str(i).zfill(6) + '\n')
-file.close() 
+    # Grab all files and determine training size
+    dirname = os.path.dirname(__file__)
+    
+    # Calibration path and files
+    calib_path = os.path.join(dirname, 'data/training/calib')
+    calib_files = [f for f in os.listdir(calib_path) if os.path.isfile(os.path.join(calib_path, f))]
+   
+    # Train size = min(max_size, train_ratio * total_size)
+    train_size = min(max_size, int(len(calib_files) * train_ratio))
+
+    # Write to train.txt 
+    output_file = os.path.join(dirname, 'data/training/train.txt')
+    output = open(output_file, 'w')
+    for i in range(train_size):
+            output.write(str(i).zfill(6) + '\n')
+    output.close() 
 
