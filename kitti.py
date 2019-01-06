2ab35824f258f9ce5a18c176aa243554efc13d73
Yuanchu <yuanchu.dang@gmail.com>
2019-01-06 05:32:38 +0000

    Cosmetic changes

--- kitti.py
+++ kitti.py
@@ -10,12 +10,21 @@ import torch
 import numpy as np
 import cv2
 import math
+import json 
 
 from utils import *
+
+
+# Load json configs 
+with open('config.json', 'r') as f:
+    config = json.load(f)
+boundary = config["boundary"]
+
 
 class KittiDataset(torch.utils.data.Dataset):
 
     def __init__(self, root = '/home/yd2466/', set = 'train', type = 'velodyne_train'):
+        
         self.type = type
         self.root = root
         self.data_path = os.path.join(root, 'training')
@@ -23,40 +32,31 @@ class KittiDataset(torch.utils.data.Dataset):
         self.image_path = os.path.join(self.data_path, "image_2/")
         self.calib_path = os.path.join(self.data_path, "calib/")
         self.label_path = os.path.join(self.data_path, "label_2/")
-
         with open(os.path.join(self.data_path, '%s.txt' % set)) as f:
             self.file_list = f.read().splitlines()
 
     def __getitem__(self, i):
-
+        
         lidar_file = self.lidar_path + '/' + self.file_list[i] + '.bin'
         calib_file = self.calib_path + '/' + self.file_list[i] + '.txt'
         label_file = self.label_path + '/' + self.file_list[i] + '.txt'
         image_file = self.image_path + '/' + self.file_list[i] + '.png'
-
+        
         if self.type == 'velodyne_train':
-
-            calib = load_kitti_calib(calib_file)
-
-            
+            calib = load_kitti_calib(calib_file)  
             target = get_target(label_file,calib['Tr_velo2cam'])
-            
+        
             # load point cloud data
             a = np.fromfile(lidar_file, dtype=np.float32).reshape(-1, 4)
-
-            b = removePoints(a,bc)
-
-            data = makeBVFeature(b, bc ,40/512)   # (512, 1024, 3)
-
+            b = removePoints(a, boundary)
+            data = makeBVFeature(b, boundary, 40 / 512)
             return data , target
 
         elif self.type == 'velodyne_test':
             NotImplemented
-
+        
         else:
             raise ValueError('the type invalid')
 
     def __len__(self):
         return len(self.file_list)
-
-
--- utils.py
+++ utils.py
@@ -10,16 +10,16 @@ import torch.utils.data as data
 import numpy as np
 import cv2
 import math
+import json
 
 
-# classes
-class_list = ['Car', 'Van' , 'Truck' , 'Pedestrian' , 'Person_sitting' , 'Cyclist' , 'Tram' ]
+# Load configs fron json
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-
-bc={}
-bc['minX'] = 0; bc['maxX'] = 80; bc['minY'] = -40; bc['maxY'] = 40
-bc['minZ'] =-2; bc['maxZ'] = 1.25
-
+# Classes of objects and anchors
+class_list = config["class_list"]
+anchors = config["anchors"]
 
 def removePoints(PointCloud, BoundaryCond):
     
@@ -29,128 +29,111 @@ def removePoints(PointCloud, BoundaryCond):
     minZ = BoundaryCond['minZ'] ; maxZ = BoundaryCond['maxZ']
     
     # Remove the point out of range x,y,z
-    mask = np.where((PointCloud[:, 0] >= minX) & (PointCloud[:, 0]<=maxX) & (PointCloud[:, 1] >= minY) & (PointCloud[:, 1]<=maxY) & (PointCloud[:, 2] >= minZ) & (PointCloud[:, 2]<=maxZ))
+    mask = np.where(
+            (PointCloud[:, 0] >= minX) & 
+            (PointCloud[:, 0] <= maxX) & 
+            (PointCloud[:, 1] >= minY) & 
+            (PointCloud[:, 1] <= maxY) & 
+            (PointCloud[:, 2] >= minZ) & 
+            (PointCloud[:, 2] <= maxZ)
+            )
     PointCloud = PointCloud[mask]
     return PointCloud
 
+
 def makeBVFeature(PointCloud_, BoundaryCond, Discretization):
+    
     # 1024 x 1024 x 3
-    Height = 1024+1
-    Width = 1024+1
+    Height = 1024 + 1
+    Width = 1024 + 1
 
     # Discretize Feature Map
     PointCloud = np.copy(PointCloud_)
     PointCloud[:,0] = np.int_(np.floor(PointCloud[:,0] / Discretization))
-    PointCloud[:,1] = np.int_(np.floor(PointCloud[:,1] / Discretization) + Width/2)
+    PointCloud[:,1] = np.int_(np.floor(PointCloud[:,1] / Discretization) + Width / 2)
     
     # sort-3times
-    indices = np.lexsort((-PointCloud[:,2],PointCloud[:,1],PointCloud[:,0]))
+    indices = np.lexsort((-PointCloud[:,2], PointCloud[:,1], PointCloud[:,0]))
     PointCloud = PointCloud[indices]
 
     # Height Map
-    heightMap = np.zeros((Height,Width))
+    heightMap = np.zeros((Height, Width))
 
-    _, indices = np.unique(PointCloud[:,0:2], axis=0, return_index=True)
+    _, indices = np.unique(PointCloud[:, 0:2], axis = 0, return_index = True)
     PointCloud_frac = PointCloud[indices]
-    #some important problem is image coordinate is (y,x), not (x,y)
-    heightMap[np.int_(PointCloud_frac[:,0]), np.int_(PointCloud_frac[:,1])] = PointCloud_frac[:,2]
-
+    
+    # Some important problem is image coordinate is (y,x), not (x,y)
+    heightMap[np.int_(PointCloud_frac[:, 0]), np.int_(PointCloud_frac[:, 1])] = PointCloud_frac[:, 2]
 
     # Intensity Map & DensityMap
-    intensityMap = np.zeros((Height,Width))
-    densityMap = np.zeros((Height,Width))
+    intensityMap = np.zeros((Height, Width))
+    densityMap = np.zeros((Height, Width))
     
-    _, indices, counts = np.unique(PointCloud[:,0:2], axis = 0, return_index=True,return_counts = True)
+    _, indices, counts = np.unique(PointCloud[:, 0:2], axis = 0, return_index = True, return_counts = True)
     PointCloud_top = PointCloud[indices]
+    normalizedCounts = np.minimum(1.0, np.log(counts + 1) / np.log(64))
+    intensityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 3]
+    densityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = normalizedCounts
+    RGB_Map = np.zeros((Height,Width, 3))
 
-    normalizedCounts = np.minimum(1.0, np.log(counts + 1)/np.log(64))
+    # RGB channels respectively
+    RGB_Map[:,:,0] = densityMap
+    RGB_Map[:,:,1] = heightMap
+    RGB_Map[:,:,2] = intensityMap
     
-    intensityMap[np.int_(PointCloud_top[:,0]), np.int_(PointCloud_top[:,1])] = PointCloud_top[:,3]
-    densityMap[np.int_(PointCloud_top[:,0]), np.int_(PointCloud_top[:,1])] = normalizedCounts
-    """
-    plt.imshow(densityMap[:,:])
-    plt.pause(2)
-    plt.close()
-    plt.show()
-    plt.pause(2)
-    plt.close()
-    plt.show(block=False)
-    plt.pause(2)
-    plt.close()
-    plt.imshow(intensityMap[:,:])
-    plt.show(block=False)
-    plt.pause(2)
-    plt.close()
-    """
-    RGB_Map = np.zeros((Height,Width,3))
-    RGB_Map[:,:,0] = densityMap      # r_map
-    RGB_Map[:,:,1] = heightMap       # g_map
-    RGB_Map[:,:,2] = intensityMap    # b_map
-    
-    save = np.zeros((512,1024,3))
-    save = RGB_Map[0:512,0:1024,:]
-    #misc.imsave('test_bv.png',save[::-1,::-1,:])
-    #misc.imsave('test_bv.png',save)   
+    save = np.zeros((512, 1024, 3))
+    save = RGB_Map[0:512, 0:1024, :]
     return save
 
 
-
-
-
-def get_target(label_file,Tr):
-    target = np.zeros([50, 7], dtype=np.float32)
+def get_target(label_file, Tr):
     
-    with open(label_file,'r') as f:
-        lines = f.readlines()
-
+    target = np.zeros([50, 7], dtype = np.float32)
+    with open(label_file, 'r') as f:
+        lines = f.readlines() 
     num_obj = len(lines)
-    index=0
+    index = 0
     for j in range(num_obj):
         obj = lines[j].strip().split(' ')
         obj_class = obj[0].strip()
-        #print(obj)
-
-
         if obj_class in class_list:
-             
-             t_lidar , box3d_corner = box3d_cam_to_velo(obj[8:], Tr)   # get target  3D object location x,y
-             location_x = t_lidar[0][0]          
-             location_y = t_lidar[0][1]
-             #print(t_lidar)             
 
-             if  (location_x>0) & (location_x<40) & (location_y>-40)  & (location_y<40) :
-                  #print(obj_class)
-                  target[index][2] = t_lidar[0][0]/40             # make sure target inside the covering area (0,1)
-                  target[index][1] = (t_lidar[0][1]+40)/80             ## we should put this in [0,1] ,so divide max_size  80 m
+            # Get target 3D object location x, y
+            t_lidar , box3d_corner = box3d_cam_to_velo(obj[8:], Tr)
+            location_x = t_lidar[0][0]          
+            location_y = t_lidar[0][1]            
+            
+            if (location_x > 0) & (location_x < 40) & (location_y > -40) & (location_y < 40):
+                
+                # Make sure target inside the covering area (0,1)
+                target[index][2] = t_lidar[0][0] / 40
+                
+                # Should put this in [0,1] ,so divide max_size 80 m
+                target[index][1] = (t_lidar[0][1]+40)/80
+                obj_width = obj[9].strip()
+                obj_length = obj[10].strip()
+                target[index][3] = float(obj_width) / 80
+                target[index][4] = float(obj_length) / 40
 
+                # Get target Observation angle of object, ranging [-pi .. pi]
+                obj_alpha = obj[3].strip()
 
-                  obj_width  = obj[9].strip()
-                  obj_length = obj[10].strip()
-                  target[index][3]=float(obj_width)/80
-                  target[index][4]=float(obj_length)/40     # get target width ,length
+                # Im axis
+                target[index][5] = math.sin(float(obj_alpha))
 
-
-                  obj_alpha = obj[3].strip()            # get target Observation angle of object, ranging [-pi..pi]
-                  target[index][5]=math.sin(float(obj_alpha))    #complex YOLO   Im
-                  target[index][6]=math.cos(float(obj_alpha))    #complex YOLO   Re
-
-                  #print(np.arctan2(target[0][4],target[0][5]))
-    
-                  for i in range(len(class_list)):
-                       if obj_class == class_list[i]:     # get target class
-                              target[index][0]=i
-                  index=index+1
-
+                # Re axis
+                target[index][6] = math.cos(float(obj_alpha))
+                for i in range(len(class_list)):
+                    if obj_class == class_list[i]:
+                        target[index][0] = i
+                index = index + 1
     return target
-
-
-
 
 
 def box3d_cam_to_velo(box3d, Tr):
 
     def project_cam2velo(cam, Tr):
-        T = np.zeros([4, 4], dtype=np.float32)
+        T = np.zeros([4, 4], dtype = np.float32)
         T[:3, :] = Tr
         T[3, 3] = 1
         T_inv = np.linalg.inv(T)
@@ -160,41 +143,30 @@ def box3d_cam_to_velo(box3d, Tr):
 
     def ry_to_rz(ry):
         angle = -ry - np.pi / 2
-
         if angle >= np.pi:
             angle -= np.pi
         if angle < -np.pi:
-            angle = 2*np.pi + angle
-
+            angle = 2 * np.pi + angle
         return angle
 
-    h,w,l,tx,ty,tz,ry = [float(i) for i in box3d]
+    h, w, l, tx, ty, tz, ry = [float(i) for i in box3d]
     cam = np.ones([4, 1])
     cam[0] = tx
     cam[1] = ty
     cam[2] = tz
     t_lidar = project_cam2velo(cam, Tr)
-
     Box = np.array([[-l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2],
                     [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2],
                     [0, 0, 0, 0, h, h, h, h]])
-
     rz = ry_to_rz(ry)
-
     rotMat = np.array([
         [np.cos(rz), -np.sin(rz), 0.0],
         [np.sin(rz), np.cos(rz), 0.0],
         [0.0, 0.0, 1.0]])
-
     velo_box = np.dot(rotMat, Box)
-
     cornerPosInVelo = velo_box + np.tile(t_lidar, (8, 1)).T
-
     box3d_corner = cornerPosInVelo.transpose()
-
     return t_lidar , box3d_corner.astype(np.float32)
-
-
 
 
 def load_kitti_calib(calib_file):
@@ -204,7 +176,6 @@ def load_kitti_calib(calib_file):
     with open(calib_file) as fi:
         lines = fi.readlines()
         assert (len(lines) == 8)
-
     obj = lines[0].strip().split(' ')[1:]
     P0 = np.array(obj, dtype=np.float32)
     obj = lines[1].strip().split(' ')[1:]
@@ -219,20 +190,12 @@ def load_kitti_calib(calib_file):
     Tr_velo_to_cam = np.array(obj, dtype=np.float32)
     obj = lines[6].strip().split(' ')[1:]
     Tr_imu_to_velo = np.array(obj, dtype=np.float32)
-
     return {'P2': P2.reshape(3, 4),
             'R0': R0.reshape(3, 3),
             'Tr_velo2cam': Tr_velo_to_cam.reshape(3, 4)}
 
 
-
-
-
-
-anchors = [1.08,1.19, 3.42,4.41, 6.63,11.38, 9.42,5.11, 16.62,10.52]
-
-
-def bbox_iou(box1, box2, x1y1x2y2=True):
+def bbox_iou(box1, box2, x1y1x2y2 = True):
     if x1y1x2y2:
         mx = min(box1[0], box2[0])
         Mx = max(box1[2], box2[2])
@@ -243,10 +206,10 @@ def bbox_iou(box1, box2, x1y1x2y2=True):
         w2 = box2[2] - box2[0]
         h2 = box2[3] - box2[1]
     else:
-        mx = min(box1[0]-box1[2]/2.0, box2[0]-box2[2]/2.0)
-        Mx = max(box1[0]+box1[2]/2.0, box2[0]+box2[2]/2.0)
-        my = min(box1[1]-box1[3]/2.0, box2[1]-box2[3]/2.0)
-        My = max(box1[1]+box1[3]/2.0, box2[1]+box2[3]/2.0)
+        mx = min(box1[0] - box1[2] / 2.0, box2[0] - box2[2] / 2.0)
+        Mx = max(box1[0] + box1[2] / 2.0, box2[0] + box2[2] / 2.0)
+        my = min(box1[1] - box1[3] / 2.0, box2[1] - box2[3] / 2.0)
+        My = max(box1[1] + box1[3] / 2.0, box2[1] + box2[3] / 2.0)
         w1 = box1[2]
         h1 = box1[3]
         w2 = box2[2]
@@ -258,15 +221,14 @@ def bbox_iou(box1, box2, x1y1x2y2=True):
     carea = 0
     if cw <= 0 or ch <= 0:
         return 0.0
-
     area1 = w1 * h1
     area2 = w2 * h2
     carea = cw * ch
     uarea = area1 + area2 - carea
-    # import pdb; pdb.set_trace()
-    return carea/uarea
+    return carea / uarea
 
-def bbox_ious(boxes1, boxes2, x1y1x2y2=True):
+
+def bbox_ious(boxes1, boxes2, x1y1x2y2 = True):
     if x1y1x2y2:
         mx = torch.min(boxes1[0], boxes2[0])
         Mx = torch.max(boxes1[2], boxes2[2])
@@ -277,10 +239,10 @@ def bbox_ious(boxes1, boxes2, x1y1x2y2=True):
         w2 = boxes2[2] - boxes2[0]
         h2 = boxes2[3] - boxes2[1]
     else:
-        mx = torch.min(boxes1[0]-boxes1[2]/2.0, boxes2[0]-boxes2[2]/2.0)
-        Mx = torch.max(boxes1[0]+boxes1[2]/2.0, boxes2[0]+boxes2[2]/2.0)
-        my = torch.min(boxes1[1]-boxes1[3]/2.0, boxes2[1]-boxes2[3]/2.0)
-        My = torch.max(boxes1[1]+boxes1[3]/2.0, boxes2[1]+boxes2[3]/2.0)
+        mx = torch.min(boxes1[0] - boxes1[2] / 2.0, boxes2[0] - boxes2[2] / 2.0)
+        Mx = torch.max(boxes1[0] + boxes1[2] / 2.0, boxes2[0] + boxes2[2] / 2.0)
+        my = torch.min(boxes1[1] - boxes1[3] / 2.0, boxes2[1] - boxes2[3] / 2.0)
+        My = torch.max(boxes1[1] + boxes1[3] / 2.0, boxes2[1] + boxes2[3] / 2.0)
         w1 = boxes1[2]
         h1 = boxes1[3]
         w2 = boxes2[2]
@@ -295,22 +257,16 @@ def bbox_ious(boxes1, boxes2, x1y1x2y2=True):
     carea = cw * ch
     carea[mask] = 0
     uarea = area1 + area2 - carea
-    return carea/uarea
-
-
-
-
+    return carea / uarea
 
 
 def nms(boxes, nms_thresh):
     if len(boxes) == 0:
         return boxes
-
     det_confs = torch.zeros(len(boxes))
     for i in range(len(boxes)):
-        det_confs[i] = 1-boxes[i][4]                
-
-    _,sortIds = torch.sort(det_confs)
+        det_confs[i] = 1 - boxes[i][4]                
+    _, sortIds = torch.sort(det_confs)
     out_boxes = []
     for i in range(len(boxes)):
         box_i = boxes[sortIds[i]]
@@ -318,15 +274,9 @@ def nms(boxes, nms_thresh):
             out_boxes.append(box_i)
             for j in range(i+1, len(boxes)):
                 box_j = boxes[sortIds[j]]
-                if bbox_iou(box_i, box_j, x1y1x2y2=False) > nms_thresh:
-                    #print(box_i, box_j, bbox_iou(box_i, box_j, x1y1x2y2=False))
+                if bbox_iou(box_i, box_j, x1y1x2y2 = False) > nms_thresh:
                     box_j[4] = 0
     return out_boxes
-
-
-
-
-
 
 
 def convert2cpu(gpu_matrix):
@@ -335,7 +285,3 @@ def convert2cpu(gpu_matrix):
 
 def convert2cpu_long(gpu_matrix):
     return torch.LongTensor(gpu_matrix.size()).copy_(gpu_matrix)
-
-
-
-
